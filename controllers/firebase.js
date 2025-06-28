0ca0d2fb7c258529f2b7231b37605765d6912990
Sohj_25 <125814848+Jhosadac@users.noreply.github.com>
2025-06-28 00:37:46 -0500

    Create firebase.js
    creacion de firebase.js

---
+++ controllers/firebase.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyApq_K7pJmtrLNZcewzxaVXWnVf4Sxynv4",
+    authDomain: "shumak-ware.firebaseapp.com",
+    databaseURL: "https://shumak-ware-default-rtdb.firebaseio.com",
+    projectId: "shumak-ware",
+    storageBucket: "shumak-ware.firebasestorage.app",
+    messagingSenderId: "64196160181",
+    appId: "1:64196160181:web:76852cef50c12a498e6cfc",
+    measurementId: "G-8L85H7B12E"
+  };
+  const app = initializeApp(firebaseConfig);
